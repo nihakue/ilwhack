db216c69aedf373623d7dd16d76042470937c8b6
nihakue <walden.gabrielw@gmail.com>
2014-02-21 16:54:13 +0000

    added more functionality to stats.py

--- stats.py
+++ stats.py
@@ -2,6 +2,7 @@
 
 from pymongo import MongoClient
 import nlp_toolkit as nlp
+import pickle
 from nltk.corpus import stopwords
 import nltk.data
 import nltk
@@ -10,7 +11,7 @@ import pdb
 import itertools
 
 stops = [w for w in stopwords.words('english')]
-tagger = nltk.data.load("taggers/treebank_brill_aubt.pickle")
+tagger = pickle.load(open("treebank_brill_aubt.pickle"))
 
 
 def main():
@@ -24,45 +25,69 @@ def main():
         name = msp['name']
         print 'name: %s' % name
         print '%.2f' % (1.0 * i / total)
-        msp_speeches = speeches.find({'name': name})
-        if not msp_speeches.count():
-            continue
-        #We have to somehow clean the unicode characters. :(
-        words = [word.encode('ascii', 'ignore')
-                    for entry in msp_speeches
-                    for word in entry['speech'].split()]
 
-
-        awl = nlp.avg_word_length(words, punctuation=False)
-        msps.update({'name': name}, {'$set': {'awl': awl}})
-
-        print 'avg_word_length: %.2f' % awl
-
-        tagged_words = tagger.tag(words)
-        nouns = [n for (n, pos) in tagged_words if 'NN' in pos]
-        verbs = [v for (v, pos) in tagged_words if 'VBD' in pos]
-        nf = nlp.word_freq(nouns, punctuation=False)
-        vf = nlp.word_freq(verbs, punctuation=False)
-        top_ten_nouns = nf.items()[:10]
-        top_ten_verbs = vf.items()[:10]
-        msps.update({'name': name}, {'$set': {'ttn': top_ten_nouns}})
-        msps.update({'name': name}, {'$set': {'ttv': top_ten_verbs}})
-
-
-
-        #Now let's compute happiness
-        pos = speeches.find({'name': name, 'mood': 'pos'})
-        neg = speeches.find({'name': name, 'mood': 'neg'})
-        #we want to range between 1 and 10 inclusive
-        try:
-            overall_happiness = ((1.0 * pos.count() / (pos.count() + neg.count())) * 10)
-        except ZeroDivisionError:
+        #We're not messing with updates right now, so skip if populated
+        if msp.has_key('awl'):
             pass
         else:
-            print 'overall: %.2f' % overall_happiness
-            msps.update({'name': name}, {'$set': {'overall_mood': overall_happiness}})
+            msp_speeches = speeches.find({'name': name})
+            if not msp_speeches.count():
+                continue
+            #We have to somehow clean the unicode characters. :(
+            words = [word.encode('ascii', 'ignore')
+                        for entry in msp_speeches
+                        for word in entry['speech'].split()]
 
 
+            awl = nlp.avg_word_length(words, punctuation=False)
+            msps.update({'name': name}, {'$set': {'awl': awl}})
+
+            print 'avg_word_length: %.2f' % awl
+
+            tagged_words = tagger.tag(words)
+            nouns = [n for (n, pos) in tagged_words if 'NN' in pos]
+            verbs = [v for (v, pos) in tagged_words if 'VBD' in pos]
+            nf = nlp.word_freq(nouns, punctuation=False)
+            vf = nlp.word_freq(verbs, punctuation=False)
+            top_ten_nouns = nf.items()[:10]
+            top_ten_verbs = vf.items()[:10]
+            msps.update({'name': name}, {'$set': {'ttn': top_ten_nouns}})
+            msps.update({'name': name}, {'$set': {'ttv': top_ten_verbs}})
+
+
+
+            #Now let's compute happiness
+            pos = speeches.find({'name': name, 'mood': 'pos'})
+            neg = speeches.find({'name': name, 'mood': 'neg'})
+            #we want to range between 1 and 10 inclusive
+            try:
+                overall_happiness = ((1.0 * pos.count() / (pos.count() + neg.count())) * 10)
+            except ZeroDivisionError:
+                pass
+            else:
+                print 'overall: %.2f' % overall_happiness
+                msps.update({'name': name}, {'$set': {'overall_mood': overall_happiness}})
+        #Compute month happiness
+
+        if msp.has_key('month_moods'):
+            pass
+        else:
+
+            last_five_months = ['October',
+            'November', 'December', 'January', 'February']
+            for month in last_five_months:
+                mon_pos = speeches.find({'name': name,
+                                            'mood': 'pos',
+                                            'date':{'$regex': month}})
+                mon_neg =   speeches.find({'name': name,
+                                            'mood': 'neg',
+                                            'date':{'$regex': month}})
+                try:
+                    month_happiness = ((1.0 * mon_pos.count() / (mon_pos.count() + mon_neg.count())) * 10)
+                    msps.update({'name': name},
+                            {'$push': {'month_moods': month_happiness}})
+                except:
+                    pass
         #Now let's compute happiness for each topic
         # topics = msp_speeches.distinct('topic')
         # for topic in topics:
